ed900b2de25b8ae334c85a563847cb2da21ad76a
akash vaidya <99950008+Vaidyaakash@users.noreply.github.com>
2022-06-26 14:45:24 +0530

    Update Sports.js

--- src/Pages/Sports.js
+++ src/Pages/Sports.js
@@ -6,48 +6,52 @@ import Block3 from '../Blocks/Block3';
 const Sports = () => {
   const [response2] = useContext(store)
   return (
-    <div className='response2'>
-      <div className='bollywood'>
-        <h1 className='latest'>Sports</h1>
-        {response2.filter((e) => (e.category === "Sports")).map((c) => {
-          return (
-            <Block2
-              ids={c.id}
-              imgurl={c.image}
-              description={c.description}
-              category={c.category}
-              date={c.date}
-              title={c.title} />)
-        })}
-      </div>
-      <div className='bolly_post'>
-        <h1 className='latest'>Top Posts</h1>
 
-        {response2.filter((e) => (e.category === "Sports" && e.id === "33")).map((c) => {
-          return (<div className='bolly_right'>
-            <img className='bolly_img' src={c.image} alt="" />
-            <h2>{c.title}</h2>
-            <div className='bolly_cat'>
-              <h2>{c.category}</h2>
-              <p> {c.date}</p>
+    <div id='res'>
+      <div className='result'>
+        <div className='bollywood'>
+          <h1 className='latest_1'>Sports</h1>
+          {response2.filter((e) => (e.category === "Sports")).map((c) => {
+            return (
+              <Block2
+                ids={c.id}
+                imgurl={c.image}
+                description={c.description}
+                category={c.category}
+                date={c.date}
+                title={c.title} />)
+          })}
+        </div>
+        <div className='bolly_post'>
+          <h1 className='latest_2'>Top Posts</h1>
+
+          {response2.filter((e) => (e.category === "Sports" && e.id === "33")).map((c) => {
+            return (<div className='bolly_right'>
+              <img className='bolly_img' src={c.image} alt="" />
+              <h2>{c.title}</h2>
+              <div className='bolly_cat'>
+                <h2>{c.category}</h2>
+                <p> {c.date}</p>
+              </div>
             </div>
-          </div>
-          )
-        })}
+            )
+          })}
 
-        {response2.filter((e) => (e.category === "Sports")).map((c) => {
-          return (
-            <Block3
-              ids={c.id}
-              imgUrl={c.image}
-              title={c.title}
-              date={c.date}
-              description={c.description}
-              category={c.category}
-            />)
-        })}
-        <div className='adv'>Advertisement</div>
+          {response2.filter((e) => (e.category === "Sports")).map((c) => {
+            return (
+              <Block3
+                ids={c.id}
+                imgUrl={c.image}
+                title={c.title}
+                date={c.date}
+                description={c.description}
+                category={c.category}
+              />)
+          })}
+          <div className='adv'>Advertisement</div>
+        </div>
       </div>
+      <div></div>
     </div>
   )
 }
