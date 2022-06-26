4a7c645a244d9e4b0f910670302cd8eb6f247e81
akash vaidya <99950008+Vaidyaakash@users.noreply.github.com>
2022-06-26 14:35:47 +0530

    Update Block2.js

--- src/Blocks/Block2.js
+++ src/Blocks/Block2.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Block2 = (props) => {
     const { imgurl, title, description, category, date, ids } = props;
     return (
-        <Link to={`/${category}/${ids}`} state={{title:title, img:imgurl, description:description, id:ids, date:date, category:category}} >
+        <Link to={`/${category}/${ids}`} state={{ title: title, img: imgurl, description: description, id: ids, date: date, category: category }} >   {/* passing props to the next component */}
             <div className='block2'>
                 <img className='block2_img' src={imgurl} alt="Not found"/> 
                 <div className='block2_data'>
                     <h2 className='block2_title'>{title}</h2> 
                     <p className='block2_desc' >{description}</p>
                     <span className='block2_flex'>
-                        <b>{category}</b> <p>{date}</p>
+                        <span>{category}</span> <p>{date}</p>
                     </span>
                 </div>
             </div>
